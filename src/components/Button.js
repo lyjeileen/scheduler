a03dadcf835b51bd8b773f66e29a424a11f8da36
lyjeileen <lyjeileen@gmail.com>
2022-11-01 15:46:01 -0700

    add props to change css styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
-import React from "react";
-
-import "components/Button.scss";
-
+import React from 'react';
+import 'components/Button.scss';
+import classNames from 'classnames';
 export default function Button(props) {
-   return <></>;
+  let buttonClass = 'button';
+  if (props.confirm) {
+    buttonClass += ' button--confirm';
+  }
+  if (props.danger) {
+    buttonClass += ' button--danger';
+  }
+
+  return (
+    <button
+      disabled={props.disabled}
+      onClick={props.onClick}
+      className={buttonClass}
+    >
+      {props.children}
+    </button>
+  );
 }
