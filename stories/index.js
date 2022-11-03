7e066c0514f4606a39d5f19f4e38e55c8804ccd5
lyjeileen <lyjeileen@gmail.com>
2022-11-02 20:08:43 -0700

    Add stories for Status

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
 
 storiesOf('Button', module)
   .addParameters({
@@ -139,6 +140,7 @@ storiesOf('Appointment', module)
   .add('Appointment with Time', () => <Appointment time="12pm" />)
   .add('Header', () => <Header time="12pm" />)
   .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Status', () => <Status message="Deleting" />)
   .add('Confirm', () => (
     <Confirm
       message="Delete the appointment?"
