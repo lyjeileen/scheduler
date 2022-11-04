9b64cb9a7da85adb93dcc883468f99c23ce06700
lyjeileen <lyjeileen@gmail.com>
2022-11-03 23:47:39 -0700

    Use axios to fetch /api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import DayList from './DayList';
 import 'components/Application.scss';
 import Appointment from 'components/Appointment';
-
-const days = [
-  {
-    id: 1,
-    name: 'Monday',
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: 'Tuesday',
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: 'Wednesday',
-    spots: 0,
-  },
-];
 
 const appointments = {
   1: {
@@ -62,10 +45,13 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
   const appointmentArray = Object.values(appointments).map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
-
+  useEffect(() => {
+    axios.get('/api/days').then((response) => setDays(response.data));
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
