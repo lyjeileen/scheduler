750fab0e5f496c2db6d7ebe0d233b23817508a3f
lyjeileen <lyjeileen@gmail.com>
2022-11-07 14:05:09 -0800

    Add saving mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,11 +4,13 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 import Form from './Form';
+import Status from './Status';
 import useVisualMode from 'hooks/useVisualMode';
 
 const EMPTY = 'EMPTY';
 const SHOW = 'SHOW';
 const CREATE = 'CREATE';
+const SAVING = 'SAVING';
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -20,14 +22,15 @@ export default function Appointment(props) {
       student: name,
       interviewer,
     };
-    props.bookInterview(interview);
-    transition(SHOW);
+    transition(SAVING);
+    props.bookInterview(interview).then(() => transition(SHOW));
   }
 
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SAVING && <Status message={SAVING} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
