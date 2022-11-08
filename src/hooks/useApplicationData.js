05fa84aa785061157137ee25a6637177af6871ea
lyjeileen <lyjeileen@gmail.com>
2022-11-07 20:53:13 -0800

    implement spots remaining feature

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 export default function useApplicationData(initial) {
   const [state, setState] = useState({
     day: 'Monday',
@@ -10,28 +11,53 @@ export default function useApplicationData(initial) {
 
   const setDay = (day) => setState({ ...state, day });
 
+  //calculate spots and generate a new "days" object. "decrease" is added to change +/-
+  const updateSpots = (id, decrease = true) => {
+    //use id to find the interview day object
+    const interviewDay = state.days.find((day) =>
+      day.appointments.includes(id)
+    );
+
+    const updatedDay = {
+      ...interviewDay,
+      spots: decrease ? interviewDay.spots - 1 : interviewDay.spots + 1,
+    };
+
+    const updatedDays = state.days.map((singleDay) => {
+      if (singleDay.id === updatedDay.id) {
+        return updatedDay;
+      }
+      return singleDay;
+    });
+    return updatedDays;
+  };
+
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview },
     };
+
     const appointments = {
       ...state.appointments,
       [id]: appointment,
     };
 
+    const days = updateSpots(id);
+
     return axios
       .put(`/api/appointments/${id}`, { interview })
-      .then((res) => setState({ ...state, appointments }));
+      .then((res) => setState({ ...state, appointments, days }));
   }
 
   function cancelInterview(id) {
     const appointment = { ...state.appointments[id], interview: null };
     const appointments = { ...state.appointments, [id]: appointment };
+    const days = updateSpots(id, false);
 
     return axios
       .delete(`/api/appointments/${id}`)
-      .then((res) => setState({ ...state, appointments }));
+      .then((res) => setState({ ...state, appointments, days }));
   }
 
   useEffect(() => {
@@ -48,6 +74,7 @@ export default function useApplicationData(initial) {
       }));
     });
   }, []);
+
   return {
     state,
     setDay,
