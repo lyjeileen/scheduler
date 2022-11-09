6ee614ab3800a34158656d32ece7b527da1b1693
lyjeileen <lyjeileen@gmail.com>
2022-11-08 17:00:17 -0800

    Add validate function and set error

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState('');
 
   const reset = () => {
     setStudent('');
@@ -16,7 +17,18 @@ export default function Form(props) {
     props.onCancel();
   };
 
-  const save = () => props.onSave(student, interviewer);
+  const validate = () => {
+    if (student === '') {
+      setError('Student name cannot be blank');
+      return;
+    }
+    if (interviewer === null) {
+      setError('Please select an interviewer');
+      return;
+    }
+
+    props.onSave(student, interviewer);
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -32,6 +44,7 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -43,14 +56,7 @@ export default function Form(props) {
           <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button
-            confirm
-            onClick={() => {
-              if (interviewer && student) {
-                save();
-              }
-            }}
-          >
+          <Button confirm onClick={validate}>
             Save
           </Button>
         </section>
