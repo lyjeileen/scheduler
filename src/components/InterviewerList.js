e2206022e55745d4dfcf087e26200add45fe4b23
lyjeileen <lyjeileen@gmail.com>
2022-11-08 16:24:34 -0800

    Add propTypes for InterviewList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './InterviewerList.scss';
 import InterviewerListItem from './InterviewerListItem.js';
 
@@ -22,3 +23,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
